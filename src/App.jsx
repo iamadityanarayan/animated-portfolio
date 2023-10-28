30c23ae731cbe84ee31a61bcba19a2e0d06ee6a7
Adityanarayan Tiwari <adityant17121997@gmail.com>
2023-10-28 20:22:07 +0530

    Sidebar and it's components with animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import './app.scss';
 import Navbar from './components/navbar/Navbar';
 const App = () => {
-  return (<main>
-    <section>
-      <Navbar />
-    </section>
-    <section>Parallax</section>
-    <section>Services</section>
-    <section>Parallax</section>
-    <section>Portfolio1</section>
-    <section>Portfolio2</section>
-    <section>Portfolio3</section>
-    <section>Contact</section>
-  </main>);
+  return (
+    <main>
+      <section id='Homepage'>
+        <Navbar />
+      </section>
+      <section id='Service'>Parallax</section>
+      <section>Services</section>
+      <section>Parallax</section>
+      <section id='Portfolio'>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section id='Contact'>Contact</section>
+    </main>
+  );
 };
 
 export default App;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './navbar.scss';
 import { motion } from 'framer-motion';
+import Sidebar from '../sidebar/Sidebar';
 
-type Props = {};
-
-const Navbar = (props: Props) => {
+const Navbar = () => {
   return (
     <div className='navbar'>
-      {/* Sidebar */}
+      <Sidebar />
       <div className='wrapper'>
         <motion.span
           initial={{ opacity: 0, scale: 0.5 }}
